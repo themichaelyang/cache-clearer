0952f9a923b45d6a6096248bdd3da35d3acfdcbf
Michael Yang <themichaelyang@gmail.com>
2017-11-09 21:52:21 -0500

    initial commit

---
+++ background.js
@@ -0,0 +1,26 @@
+let running = {}
+
+chrome.browserAction.onClicked.addListener((tab) => {
+  if (!(tab.id in running)) {
+    running[tab.id] = false
+  }
+  running[tab.id] = !running[tab.id]
+
+  if (running[tab.id]) {
+    clear(tab.id)
+  }
+  else {
+    chrome.browserAction.setIcon({ tabId: tab.id, path: "icons/off.png" })
+  }
+})
+
+chrome.tabs.onUpdated.addListener((tabId) => {
+  if ((tabId in running) && running[tabId]) {
+    clear(tabId)
+  }
+})
+
+function clear(tabId) {
+  chrome.browserAction.setIcon({ tabId: tabId, path: "icons/on.png" })
+  chrome.tabs.executeScript({ tabId: tabId, file: 'cache-clear.js' })
+}
