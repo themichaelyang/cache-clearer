a27929004465446d7f29d473cf49956d6bb84087
Michael Yang <themichaelyang@gmail.com>
2017-11-11 01:21:28 -0500

    fixed cache clearing

--- background.js
+++ background.js
@@ -1,27 +1,38 @@
 let running = {}
+let clearing = {}
+let count = 0
 
 chrome.browserAction.onClicked.addListener((tab) => {
   if (!(tab.id in running)) {
     running[tab.id] = false
+    // count++
   }
   running[tab.id] = !running[tab.id]
 
   if (running[tab.id]) {
+    chrome.browserAction.setIcon({ tabId: tab.id, path: 'icons/on.png' })
     clear(tab.id)
   }
   else {
     chrome.browserAction.setIcon({ tabId: tab.id, path: 'icons/off.png' })
+    // if (count >= 50) { // for those who never close chrome
+      // delete running[tab.id]
+      // count--
+    // }
   }
 })
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-  if (changeInfo.status === 'complete' &&
-  (tabId in running) && running[tabId]) {
-    clear(tabId)
+chrome.webRequest.onBeforeRequest.addListener((details) => {
+  if (running[details.tabId]) {
+    clear(details.tabId)
   }
-})
+}, { urls: ['<all_urls>'] })
 
 function clear(tabId) {
-  chrome.browserAction.setIcon({ tabId: tabId, path: 'icons/on.png' })
-  chrome.browsingData.removeCache()
+  if (!(tabId in clearing)) {
+    clearing[tabId] = true
+    chrome.browsingData.removeCache({ since: 0 }, () => {
+      delete clearing[tabId]
+    })
+  }
 }
